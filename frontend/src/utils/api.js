87980255af538f00e535b81b6bc4608176099b3d
HIJHMALA JAIRAM <hijhmalajairam24@gmail.com>
2026-06-11 22:25:33 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({ baseURL: 'https://spekit-health-twin.onrender.com' })
 
 api.interceptors.request.use(config => {
   const user = JSON.parse(localStorage.getItem('ht_user') || 'null')
